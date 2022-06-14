e8b57b10d3c13f2d132f74fc3207e70dddf4b8ea
nichamon <nichamon.ve@gmail.com>
2022-06-14 15:01:31 +0700

    create search sort and filter for shelves

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const bookRepo = require("../repositories/book.repos");
+const shelfRepo = require("../repositories/shelf.repos");
 
 route.get("/", async (req, res) => {
   res.status(200).send(":)").end();
@@ -8,7 +9,8 @@ route.get("/", async (req, res) => {
 
 route.get("/books", async (req, res, next) => {
   try {
-    const result = await bookRepo.getAll();
+    const { page, size } = req.query;
+    const result = await bookRepo.getAll(page || 1,size || 25);
     res.status(200).send(result).end();
   } catch (err) {
     next(err);
@@ -68,10 +70,10 @@ route.get("/books/filter", async (req, res, next) => {
   try {
     const { startDate, endDate } = req.query;
     if (!startDate) {
-      throw { statusCode:400, message:"require startDate"}
+      throw { statusCode: 400, message: "require startDate" };
     } else if (!endDate) {
-      throw { statusCode:400, message:"require endDate"}
-    } 
+      throw { statusCode: 400, message: "require endDate" };
+    }
     const result = await bookRepo.filterDate(startDate, endDate);
     res.status(200).send(result).end();
   } catch (err) {
@@ -85,16 +87,16 @@ route.get("/books/sort", async (req, res, next) => {
     let result;
     let order;
 
-    if (query.order === 'asc') {
+    if (query.order === "asc") {
       order = 1;
-    } else if (query.order === 'desc'){
+    } else if (query.order === "desc") {
       order = -1;
     } else {
-      throw { statusCode:400, message:"invalid order"}
+      throw { statusCode: 400, message: "invalid order" };
     }
 
     if (!query.type) {
-      throw { statusCode:400, message:"require type"}
+      throw { statusCode: 400, message: "require type" };
     }
 
     switch (query.type) {
@@ -121,6 +123,80 @@ route.get("/books/sort", async (req, res, next) => {
 route.get("/books/group/price", async (req, res, next) => {
   try {
     const result = await bookRepo.groupByPrice();
+    res.status(200).send(result).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+route.post("/shelf", async (req, res, next) => {
+  try {
+    const { name, column, isActive, books, size } = req.body;
+    const sampleBooks = await bookRepo.sampleBooks(size);
+    const shelf = {
+      name: name,
+      column: column,
+      isActive: isActive,
+      books: sampleBooks,
+    };
+    const result = await shelfRepo.create(shelf);
+    res.status(201).send(result).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+route.get("/shelves/search", async (req, res, next) => {
+  try {
+    const { name } = req.query;
+    const result = await shelfRepo.findByName(name);
+    res.status(200).send(result).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+route.get("/shelves/filter", async (req, res, next) => {
+  try {
+    const { isActive } = req.query;
+    const isTrue = (isActive === 'true');
+    const result = await shelfRepo.filterActive(isTrue);
+    res.status(200).send(result).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+route.get("/shelves/sort", async (req, res, next) => {
+  try {
+    const query = req.query;
+    let result;
+    let order;
+
+    if (query.order === "asc") {
+      order = 1;
+    } else if (query.order === "desc") {
+      order = -1;
+    } else {
+      throw { statusCode: 400, message: "invalid order" };
+    }
+
+    if (!query.type) {
+      throw { statusCode: 400, message: "require type" };
+    }
+
+    switch (query.type) {
+      case "books":
+        result = await shelfRepo.sortByBooks(order);
+        break;
+      case "name":
+        result = await shelfRepo.sortByName(order);
+        break;
+      case "column":
+        result = await shelfRepo.sortByColumn(order);
+        break;
+    }
+
     res.status(200).send(result).end();
   } catch (err) {
     next(err);
