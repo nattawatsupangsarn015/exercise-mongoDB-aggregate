64c6a6aa01c8ffebfee5091c7dff5b11a0635082
nichamon <nichamon.ve@gmail.com>
2022-06-14 13:12:00 +0700

    group by price function

--- src/repositories/book.repos.js
+++ src/repositories/book.repos.js
@@ -53,7 +53,44 @@ module.exports.filterDate = async (startDate, endDate) => {
 module.exports.groupByPrice = async () => {
   return await model.aggregate([
     {
-      $project: { _id: 1},
+      $facet: {
+        "0-100": [
+          {
+            $match: {
+              price: { $gt: 0, $lt: 101 },
+            },
+          },
+          { $project: { _id: 1 } },
+          { $sort: { price: 1 } },
+        ],
+        "101-200": [
+          {
+            $match: {
+              price: { $gt: 101, $lt: 201 },
+            },
+          },
+          { $project: { _id: 1 } },
+          { $sort: { price: 1, price: 1 } },
+        ],
+        "201-300": [
+          {
+            $match: {
+              price: { $gt: 201, $lt: 301 },
+            },
+          },
+          { $project: { _id: 1 } },
+          { $sort: { price: 1 } },
+        ],
+        "301-400": [
+          {
+            $match: {
+              price: { $gt: 301, $lt: 401 },
+            },
+          },
+          { $project: { _id: 1 } },
+          { $sort: { price: 1 } },
+        ],
+      },
     },
   ]);
 };
